301e390267af6f0eb75ad55f7e167c9d82a81e16
htotto42 <htotto42@tntech.edu>
2024-03-22 08:47:35 -0500

    Classwork 3/22/2024

--- index.js
+++ index.js
@@ -6,24 +6,62 @@ console.log("Listening on port: " + HHTP_Port);
 var app = express();
 app.use(cors());
 
+class Fruit {
+    constructor(strname, strcolor) {
+        this.name = strname;
+        this.color = strcolor;
+    }
+}
+
 var arrFruit = [];
-let objBanana = {name:'Banana', color:'Yellow'};
-let objApple = {name:'Apple', color:'Red'};
-let objGrape = {name:'Grape', color:'Purple'};
+let objBanana = new Fruit('banana','yellow')
+let objApple = new Fruit('apple','red')
+let objGrape = new Fruit('grape','purple')
 arrFruit.push(objBanana);
 arrFruit.push(objApple);
 arrFruit.push(objGrape);
+arrFruit.push(new Fruit('kiwi','green'));
 
 app.get("/", (req, res, next) => {
-    let strFruit = req.query.fruit;
-    console.log("Routed to base route");
-    console.log(strFruit);
-    arrFruit.forEach(function(thisfruit){
-        if (thisfruit.name == strFruit){
-            res.status(200).send(thisfruit);
-        }
-    });
-    res.status(200).send('{"message":"Fruit not found"}');
+    res.status(200).send(arrFruit);
+});
+
+app.post("/fruit", (req, res, next) => {
+    let strName = req.query.name;
+    let strColor = req.query.color;
+    let objFruit = new Fruit(strName, strColor);
+    arrFruit.push(objFruit);
+    res.status(201).send(arrFruit);
+});
+
+app.get("/fruit", (req, res, next) => {
+    let strName = req.query.name;
+    if(strName){
+        arrFruit.forEach(function(fruit){
+            if(fruit.name == strName){
+                res.status(200).send(fruit);
+            }
+        })
+        res.status(200).send({message: "Fruit not found"})
+    } else {
+        res.status(200).send(arrFruit);
+    }
+});
+
+app.delete("/fruit", (req, res, next) => {
+    let strName = req.query.name;
+    if(strName){
+        arrFruit.forEach(function(fruit){
+            if(fruit.name == strName){
+                arrFruit.splice(arrFruit.indexOf(fruit), 1);
+                res.status(200).send(fruit);
+            }
+        })
+        res.status(200).send({message: "Fruit not found"})
+    } else {
+        arrFruit = [];
+        res.status(200).send(arrFruit);
+    }
 });
 
 app.get("/hello", (req, res, next) => {
