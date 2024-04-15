d5c9976fbec14f32354652ca24185dcd7afe17fc
htotto42 <htotto42@tntech.edu>
2024-04-15 07:41:47 -0500

    classwork

--- index.js
+++ index.js
@@ -4,99 +4,105 @@ const {v4:uuidv4} = require('uuid');
 const sqlite3 = require('sqlite3').verbose();
 const dbSource = "node.db";
 const db = new sqlite3.Database(dbSource);
+const HTTP_PORT = 8000;
 
-const HHTP_Port = 8000;
-console.log("Listening on port: " + HHTP_Port);
+console.log("Listening on port " + HTTP_PORT);
 var app = express();
 app.use(cors());
 
-class Fruit {
-    constructor(strname, strcolor) {
-        this.name = strname;
-        this.color = strcolor;
+//This is a seperate way than shown in class. It is a built in alternative to body-parser
+app.use(express.json());
+
+// Task 1 Implement CRUD Operations to manage room reservations
+// Get 
+app.get("/reservations", (req,res,next) => {
+    let strCommand = "SELECT * FROM tblReservations";
+    db.all(strCommand,[],(err,rows) => {
+        if(err){
+            res.status(400).json({error:err.message})
+        } else {
+            res.status(200).json(rows)
+        }
+    })
+})
+
+// Get by ID
+app.get("/reservations/:id", (req,res,next) => {  
+    let strID = req.params.id;
+    if (strID){
+        let strCommand = "SELECT * FROM tblReservations WHERE ID = ?";
+        let arrParameters = [strID];
+        db.get(strCommand,arrParameters,(err,row) => {
+            if(err){
+                res.status(400).json({error:err.message})
+            } else {
+                res.status(200).json(row)
+            }
+        })
+    } else {
+        res.status(400).json({error:"No ID provided"})
+    }
+})
+
+// Delete
+app.delete("/reservations/:id", (req,res,next) => {
+    let strID = req.params.id;
+    if (strName){
+        let strCommand = "DELETE FROM tblReservations WHERE ID = ?";
+        let arrParameters = [strID];
+        db.run(strCommand,arrParameters,function(err,result){
+            if(err){
+                res.status(400).json({error:err.message})
+            } else {
+                res.status(200).json({message:"deleted"})
+            }
+        })
+    } else {
+        res.status(400).json({error:"No ID provided"})
+    }
+})
+
+// Post
+app.post("/reservations", (req,res,next) => {
+    let strID = uuidv4();
+    let strRoomNumber = req.body.RoomNumber;
+    let strDate = req.body.Date;
+    let strStartTime = req.body.StartTime;
+    let strEndTime = req.body.EndTime;
+    let strReservedBy = req.body.ReservedBy;
+    let strCommand = "INSERT INTO tblReservations VALUES(?,?,?,?,?,?)";
+    if(strRoomNumber && strDate && strStartTime && strEndTime && strReservedBy){
+        let arrParameters = [strID, strRoomNumber, strDate, strStartTime, strEndTime, strReservedBy];
+        db.run(strCommand,arrParameters, function(err,result){
+            if(err){
+                res.status(400).json({error:err.message})
+            } else {
+                res.status(201).json({message:"success"})
+            }
+        })
+    } else {
+        res.status(400).json({error:"Not all parameters provided"})
+    }
+})
+
+
+// Task 2: Custom Class Implementation
+class InstallationInfo {
+    constructor(strName, strprimaryColor, strAddress){
+        this.name = strName
+        this.primaryColor = strprimaryColor;
+        this.address = strAddress;
     }
 }
 
-let strID = uuidv4();
-var arrFruit = [];
-let objBanana = new Fruit('banana','yellow')
-let objApple = new Fruit('apple','red')
-let objGrape = new Fruit('grape','purple')
-arrFruit.push(objBanana);
-arrFruit.push(objApple);
-arrFruit.push(objGrape);
-arrFruit.push(new Fruit('kiwi','green'));
+// Create an instance with the values given in the test.
+var arrInstallation = [];
+let objCookevillePolice = new InstallationInfo('Cookeville Police', 'blue', '1019 Neal Street');
+arrInstallation.push(objCookevillePolice);
 
-app.get("/", (req, res, next) => {
-    res.status(200).send(arrFruit);
-});
+//Get
+app.get("/installinfo", (req,res,next) => { 
+    res.status(200).send(arrInstallation);
+})
 
-app.post("/fruit", (req, res, next) => {
-    let strName = req.query.name;
-    let strColor = req.query.color;
-    let strCommand = "INSERT INTO tblFRUIT (name, color) VALUES (?,?)";
-    if(strName && strColor){
-        let arrParameters = [strName, strColor];
-        let objFruit = new Fruit(strName, strColor);
-        db.run(strCommand, arrParameters, function(error, result){
-            if(error){
-                res.status(400).json({error: error.message});
-            } else {
-                res.status(201).json({message: "success", fruit: objFruit})
-            }
-        })
-    } else {
-        res.status(400).json({error: "Not all parameters provided"})
-    }
-    
-});
-
-app.get("/fruit/:name", (req, res, next) => {
-    let strName = req.params.name;
-    if(strName){
-        let strCommand = "SELECT * FROM tblFRUIT WHERE name = ?";
-        let arrParameters = [strName];
-        db.all(strCommand,arrParameters,(err, rows) => {
-            if(err){
-                res.status(400).json({error:err.message});
-            } else {
-                if(rows.length < 1){
-                    res.status(200).json({message: "error: not found"});
-                } else {
-                    res.status(200).json({message: "success", fruit:rows});
-                }
-            }
-        })
-    } else {
-        res.status(400).json({error: "No fruit name provided"});
-    }
-});
-
-app.delete("/fruit", (req, res, next) => {
-    let strName = req.query.name;
-    if(strName){
-        arrFruit.forEach(function(fruit){
-            if(fruit.name == strName){
-                arrFruit.splice(arrFruit.indexOf(fruit), 1);
-                res.status(200).send(fruit);
-            }
-        })
-        res.status(200).send({message: "Fruit not found"})
-    } else {
-        arrFruit = [];
-        res.status(200).send(arrFruit);
-    }
-});
-
-app.get("/hello", (req, res, next) => {
-    let strCommand = 'SELECT * FROM tblFRUIT';
-    db.all(strCommand, (err, rows) => {
-        if(err){
-            res.status(400).json({error:err.message});
-        } else {
-            res.status(200).json({message: "success", fruit:rows});
-        }
-    });
-});
-
-app.listen(HHTP_Port);
+app.listen(HTTP_PORT);
